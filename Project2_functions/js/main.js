011b33ea186f28b66c0ec2db543d3df2f9fdada9
Noorahmad Noor <102923335+noorahmad2022@users.noreply.github.com>
2022-04-28 15:53:46 -0700

    Add files via upload
    Assignment Submission # 2

---
+++ Project2_functions/js/main.js
@@ -0,0 +1,11 @@
+function printRedText(){
+    var str="This is Red text";
+    var prn=str.fontcolor("Red");
+    document.getElementById("redText").innerHTML=prn;
+}
+
+function myFunction (){
+    var sentance = "I am learning ";
+    sentance += "a lot from this course!";
+    document.getElementById("con").innerHTML=sentance;
+}
