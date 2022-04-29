f8d4ddb8e23a3a2d7ff98113223916079f7c0a18
Noorahmad Noor <102923335+noorahmad2022@users.noreply.github.com>
2022-04-29 15:40:57 -0700

    Add files via upload
    update

--- Project2_functions/js/main.js
+++ Project2_functions/js/main.js
@@ -9,3 +9,14 @@ function myFunction (){
     sentance += "a lot from this course!";
     document.getElementById("con").innerHTML=sentance;
 }
+
+function sumOfTwoNumber(num1, num2){
+    return num1 + num2;     // The function returns the product of p1 and p2
+}
+document.getElementById ("Calculation").innerHTML=sumOfTwoNumber(20, 10);
+
+
+function additionFunction(){
+    var addition = 2 + 8;
+    document.getElementById ("math").innerHTML = addition;
+}
---
+++ Project3_math_operators/js/main.js
@@ -0,0 +1,21 @@
+
+function additionFunction(){
+    var addition = 2 + 8;
+    document.getElementById ("Math").innerHTML = "2 + 8 = " + addition;
+}
+
+
+function subtractionFunction(num1, num2){
+    return num1 - num2;     // The function returns the product of p1 and p2
+}
+document.getElementById ("substract").innerHTML= "20 - 10 = " + subtractionFunction(20, 10);
+
+function MultiPlyFunction(){
+    var Multiply = 2 * 8;
+    document.getElementById ("Multiplication").innerHTML = "2 * 8 = " + Multiply;
+}
+
+function dividFunction(){
+    var divid = 200 / 8;
+    document.getElementById ("dividition").innerHTML = "200 /8 = " + divid;
+}
