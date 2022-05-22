e88da13802e619415e4807e965c81475c8494692
Noorahmad Noor <noornoorahmad694@gmail.com>
2022-05-22 07:58:08 -0700

    Assignment Submission # 6

--- Project6_ternary__operators_constructors/main.js
+++ Project6_ternary__operators_constructors/main.js
@@ -5,3 +5,14 @@ function voteAge(){
     Can_vote=(age<18)? "You are not allowed":"Your are allowed";
     document.getElementById("vote").innerHTML=Can_vote+" to vote."
 }
+
+
+function countFunction (){
+    document.getElementById("Nested_Function").innerHTML=count();
+    function count(){
+        var starting_point = 9;
+        function plus_one() {starting_point +=1;}
+        plus_one();
+        return starting_point;
+    }
+}
