c2fd612c5963672faa129cf34d20ca06a7d9e23f
Noorahmad Noor <102923335+noorahmad2022@users.noreply.github.com>
2022-05-27 12:15:59 -0700

    Assignment Submission # 8

--- Project8_string_methods/main.js
+++ Project8_string_methods/main.js
@@ -6,3 +6,58 @@ function fullSentance(){
     var whole_sentance = part1.concat(part2,part3,part4);
     document.getElementById("concatenate").innerHTML=whole_sentance;
 }
+
+function sliceMethod(){
+    var Sentennce = "All work and no play makes Johnny a dull boy.";
+    var section = Sentennce.slice(27,33);
+    document.getElementById("slice").innerHTML=section;
+}
+
+function upperCase(){
+    var name = "noorahmad";
+    var upperName = name.toUpperCase();
+    document.getElementById("Para").innerHTML=upperName;
+}
+
+function lowerCase(){
+    document.getElementById("Para").style.textTransform="lowercase";
+}
+
+function capCase(){
+    document.getElementById("Para").style.textTransform="capitalize";
+
+}
+
+function Search(){
+    var Sname = "Noorahmad Noor";
+    var searchName = Sname.search("Noor");
+    document.getElementById("Para").innerHTML=searchName;
+}
+
+function numStringMethod(){
+    var num=250;
+    document.getElementById("numToString").innerHTML=num.toString();
+}
+
+function precisionMethod(){
+    var preNum= 345998.676542327;
+    document.getElementById("precision").innerHTML=preNum.toPrecision(10);
+}
+
+function toFixe(){
+    var preNum= 345998.676547873990;
+    document.getElementById("precision").innerHTML=preNum.toFixed();
+}
+function toFixe1(){
+    var preNum= 345998.676547873990;
+    document.getElementById("precision").innerHTML=preNum.toFixed(1);
+}
+function toFixe2(){
+    var preNum= 345998.676547873990;
+    document.getElementById("precision").innerHTML=preNum.toFixed(2);
+}
+
+function valueOf(){
+    var student={Name: 'Noorahmad', LastName: 'Noor', RoleNo:'765'};
+    document.getElementById("student").innerHTML=student.valueOf();
+}
