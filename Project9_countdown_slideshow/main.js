5b34050d76bceff08999f2e8fdb17b6e5970aabe
Noorahmad Noor <102923335+noorahmad2022@users.noreply.github.com>
2022-05-27 14:36:05 -0700

    Add files via upload
    Assignment Submission # 9

---
+++ Project9_countdown_slideshow/main.js
@@ -0,0 +1,16 @@
+function countdown(){
+    var seconds = document.getElementById("seconds").value;
+
+    function tick(){
+        seconds = seconds - 1;
+        timer.innerHTML = seconds;
+        setTimeout(tick, 1000);
+      if(seconds==0){
+          alert("Time's up!");
+          location.reload();
+          
+      }  
+    }
+    tick();
+}
+
