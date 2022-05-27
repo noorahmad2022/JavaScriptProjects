dceda3aa4be61202fa497cded215043dc2204876
Noorahmad Noor <102923335+noorahmad2022@users.noreply.github.com>
2022-05-27 16:03:23 -0700

    Add files via upload

--- Project6_ternary__operators_constructors/main.js
+++ Project6_ternary__operators_constructors/main.js
@@ -5,3 +5,28 @@ function voteAge(){
     Can_vote=(age<18)? "You are not allowed":"Your are allowed";
     document.getElementById("vote").innerHTML=Can_vote+" to vote."
 }
+
+function Group(){
+    var group, result;
+
+    group=document.getElementById("group").value;
+
+    if(group<=7){
+        document.getElementById("groupResult").innerHTML= "Group C";
+
+    }
+    else if(group>7 && group<=18){
+        document.getElementById("groupResult").innerHTML= "Group B";
+
+    }
+    else if(group>18 && group<=35){
+        document.getElementById("groupResult").innerHTML= "Group A";
+
+    }
+
+    else
+    document.getElementById("groupResult").innerHTML= "Your age not matched to any group.";
+
+
+
+}
